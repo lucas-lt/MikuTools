14ad29ada71552ee3826aeda0a1afe7a9e25335f
lucas-lt <786369943@qq.com>
2024-07-05 03:19:15 +0800

    init

--- env.js
+++ env.js
@@ -4,21 +4,21 @@ export default {
     title: 'MikuTools',
     domain:
         process.env.NODE_ENV === 'development'
-            ? 'dev.miku.tools:3000'
-            : 'miku.tools',
+            ? 'dev.iae.us.kg:3000'
+            : 'tool.iae.us.kg',
     description: '一个轻量的工具集合',
     url:
         process.env.NODE_ENV === 'development'
             ? 'http://dev.miku.tools:3000'
-            : 'https://miku.tools',
+            : 'https://tool.iae.us.kg',
     apiUrl:
         process.env.NODE_ENV === 'development'
-            ? 'http://dev.miku.tools:3001'
-            : 'https://api.miku.tools',
+            ? 'http://dev.iae.us.kg:3001'
+            : 'https://tool-api.iae.us.kg',
     pinyin: pinyin,
     development: process.env.NODE_ENV === 'development',
     axios:
         process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:3001'
-            : 'https://api.miku.tools'
+            : 'https://tool-api.iae.us.kg'
 };
